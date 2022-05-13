7e1194635143df3877abe3c424367a37e6fc8433
zerotume <zerotume@gmail.com>
2022-05-13 14:51:55 -0700

    longestCommonPrefix

--- README.md
+++ README.md
@@ -13,10 +13,17 @@ Record my trip on my leetcode attempts.
 - Runtime: 302 ms, faster than 25.05% of JavaScript online submissions for Palindrome Number.
 - Memory Usage: 51.1 MB, less than 58.92% of JavaScript online submissions for Palindrome Number.
 
-from other ones solution:
-- num % 10 === 0 -> last digit is 0, return false
-- .split('').reverse().join('') and compare
+- from other ones solution:
+  - num % 10 === 0 -> last digit is 0, return false
+  - .split('').reverse().join('') and compare
+  - (I'm So Fxxxixx Sxxxxd)
 
-(I'm So Fxxxixx Sxxxxd)
+---
+
+## longestCommonPrefix
+
+- Runtime: 77 ms, faster than 64.12% of JavaScript online submissions for Longest Common Prefix.
+- Memory Usage: 43.8 MB, less than 25.47% of JavaScript online submissions for Longest Common Prefix.
+- submitted 5 times and passed wtf
 
 ---
---
+++ easy/longestCommonPrefix.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    if(strs.length === 1)return strs[0];
+    let obj = {};
+    for(let i = 0; i < strs[0].length; i++){
+        obj[strs[0].slice(0,i+1)] = true;
+    }
+    let common = "";
+    for(let i = 1; i < strs.length; i++){
+        let flag = false;
+        let subcommon = '';
+        for(let j = 0; j < strs[i].length; j++){
+            let sub = strs[i].slice(0, j+1);
+            if(obj[sub]){
+                subcommon = sub;
+                flag = true;
+            }
+        }
+        if(!flag)return '';
+        if(subcommon.length < common.length || i === 1)common = subcommon;
+    }
+    return common;
+};
+
+// Runtime: 77 ms, faster than 64.12% of JavaScript online submissions for Longest Common Prefix.
+// Memory Usage: 43.8 MB, less than 25.47% of JavaScript online submissions for Longest Common Prefix.
+// 5 submissions wtf
