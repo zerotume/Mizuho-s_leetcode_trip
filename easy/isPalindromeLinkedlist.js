05f5269b946950cd9deaccff7e05c3993c746ad6
zerotume <zerotume@gmail.com>
2022-10-24 21:37:36 -0700

    is palindrome linkedlist

---
+++ easy/isPalindromeLinkedlist.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+ var isPalindrome = function(head) {
+    if(head.next === null)return head.val;
+    let fast = head;
+    let slow = head;
+    while(fast != null && fast.next !== null){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if(fast != null){
+        slow = slow.next;
+    }
+
+    let prev = slow;
+    slow = slow.next;
+    prev.next = null;
+    while(slow){
+        let temp = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    let again = head;
+    slow = prev;
+
+    while(slow){
+        if(slow.val !== again.val)return false;
+        slow = slow.next;
+        again = again.next;
+    }
+
+    return true;
+};
+
+// Runtime: 231 ms, faster than 62.57% of JavaScript online submissions for Palindrome Linked List.
+// Memory Usage: 69.5 MB, less than 83.42% of JavaScript online submissions for Palindrome Linked List.
+// the biggest chanllenge is about how javascript is referencing the memory block
+// so the order of assigning value is important
