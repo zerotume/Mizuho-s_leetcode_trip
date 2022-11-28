7376e2fb8c61ff04ae9ad94d9873f8a7600f60be
zerotume <zerotume@gmail.com>
2022-11-27 22:14:11 -0800

    test

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-function* gen1(){
-    console.log(yield 1);
-    console.log(yield 2);
-    console.log(yield 3);
-}
-
-const i = gen1();
-
-console.log(i.next('a').value);
-console.log(i.next('b').value);
-console.log(i.next('c').value);
+for (var i = 0; i < 3; i++) {
+    setTimeout(function() { alert(i); }, 1000 + i);
+  }
