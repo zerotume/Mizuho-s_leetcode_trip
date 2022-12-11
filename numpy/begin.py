75333b57b8d76a5d33b521531605a449a7d85237
zerotume <zerotume@gmail.com>
2022-12-10 23:02:17 -0800

    real linear

--- numpy/begin.py
+++ numpy/begin.py
@@ -6,10 +6,12 @@ arr2 = np.array([[5,6],[7,8]])
 print(arr1.shape)
 s = arr1 + arr2
 d = arr1 - arr2
-p = arr1 * arr2 # linear algebra support!
+p = arr1 * arr2
+m_p = arr1.dot(arr2)# linear algebra support!
 q = arr1 / arr2 # cool!
 
 print(s)
 print(d)
 print(p)
+print(m_p)
 print(q)
