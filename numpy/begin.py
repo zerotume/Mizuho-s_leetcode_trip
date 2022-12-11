355773f73bc8763a5aeb1e8d2b8c67fade1c25ee
zerotume <zerotume@gmail.com>
2022-12-10 23:00:49 -0800

    linear algebra?

---
+++ numpy/begin.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+arr1 = np.array([[1,2],[3,4]])
+arr2 = np.array([[5,6],[7,8]])
+
+print(arr1.shape)
+s = arr1 + arr2
+d = arr1 - arr2
+p = arr1 * arr2 # linear algebra support!
+q = arr1 / arr2 # cool!
+
+print(s)
+print(d)
+print(p)
+print(q)
