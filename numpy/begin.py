a9c9808f55792bfe760235a8d78c17d271261a20
zerotume <zerotume@gmail.com>
2022-12-13 22:38:15 -0800

    6th

--- numpy/begin.py
+++ numpy/begin.py
@@ -16,5 +16,23 @@ q = arr1 / arr2 # cool!
 # print(m_p)
 # print(q)
 
-print(np.__version__)
-np.show_config()
+# print version and config
+# print(np.__version__)
+# np.show_config()
+
+# generate an empty vector with length of 10
+z = np.zeros(10)
+print(z)
+
+# memory size of an array
+print(f"{z.itemsize} for each element")
+print(f"{z.size * z.itemsize}bytes")
+
+# doc of one function
+# numpy.info(name of the function)
+# print(np.info(np.add))
+
+# an empty vector with length of 10 but 5th value is 1?
+z = np.zeros(10)
+z[4] = 1
+print(z)
