59355732cc92d8e90aa79ef1a80ed062e0e803e0
zerotume <zerotume@gmail.com>
2022-12-20 23:52:59 -0800

    number under diagonal

--- numpy/100questions.py
+++ numpy/100questions.py
@@ -70,8 +70,13 @@ import numpy as np
 # print(z)
 
 # See some results
-print(0 * np.nan)
-print(np.nan == np.nan)
-print(np.inf > np.nan)
-print(np.nan - np.nan)
-print(0.3 == 3 * 0.1) #false
+# print(0 * np.nan)
+# print(np.nan == np.nan)
+# print(np.inf > np.nan)
+# print(np.nan - np.nan)
+# print(0.3 == 3 * 0.1) #false
+
+# 5*5 matrix 1234 under diagonal
+z = np.diag(1+np.arange(4), k=-1)
+print(z)
+# get a explaination here:
