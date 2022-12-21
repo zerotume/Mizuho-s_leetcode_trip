d568ff354805f22449efe327f48dc3d9a3e76364
zerotume <zerotume@gmail.com>
2022-12-21 00:13:49 -0800

    Level Ancestor

---
+++ bigdata/levelAncestor.py
@@ -0,0 +1,23 @@
+from __future__ import annotations # allows things in class to typehint itself
+import math
+from itertools import repeat
+from typing import List
+
+# nooooooot right
+
+class SuperTree:
+
+    def __init__(self) -> None:
+        self.rad = 0
+
+
+    def set_ancestor(self, ans:List[List[int]], depth:List[int], node, len:int, n:int):
+        depth[self.rad] = 0
+        for j in range(len):
+            for i in range(n):
+                pass
+        return -1
+    pass
+
+class SuperTreeNode:
+    pass
